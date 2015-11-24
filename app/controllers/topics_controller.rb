de51b8785c4235b772a83225a51ef2edea2f6562
Kostia Kaploon <kostia@simplee.com>
2015-11-24 17:56:17 +0200

    Some refactoring

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -41,4 +41,9 @@ class TopicsController < ApplicationController
       render :edit
     end
   end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
+  end
 end
