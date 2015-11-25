4b3784e517b381746558c7d5f94bffad9f7b0928
Kostia Kaploon <kostia@simplee.com>
2015-11-25 11:50:28 +0200

    reverted test

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -8,10 +8,7 @@ class Topic < ActiveRecord::Base
     if wiki_obj && wiki_obj.text
       text = wiki_obj.text[0..800]
       text = text[0..text.rindex(/ /)-1] + "..."
-      puts "#{self.inspect}"
-      puts "#{self.methods - Object.methods}"
       self.wikipedia_text = text
-      puts "#{self.inspect}"
       self.wikipedia_link = wiki_obj.fullurl
     end
   end
